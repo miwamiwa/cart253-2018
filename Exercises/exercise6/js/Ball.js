2c7e7fcfdc37a49448bfadce94f0d77129fad2ea
sam <spchouinard@gmail.com>
2018-10-23 17:14:24 -0400

    Ex6: ball does not move along x axis.
    Problem: incorrect assignment at line 28 in ball.js: this.vx is being assigned rather than added.
    Fix: corrected "=" to "+="

--- Exercises/exercise6/js/Ball.js
+++ Exercises/exercise6/js/Ball.js
@@ -25,7 +25,8 @@ function Ball(x,y,vx,vy,size,speed) {
 Ball.prototype.update = function () {
   console.log("this works");
   // Update position with velocity
-  this.x = this.vx;
+  //////////////// FIXED
+  this.x += this.vx;
   this.y += this.vy;
 
   // Constrain y position to be on screen
